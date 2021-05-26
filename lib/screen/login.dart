0904994352052e450a18de4ec6f47aa4449b2acd
bradkim-laptop <jitae@gmail.com>
2021-05-27 02:05:21 +0900

    tired

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,12 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
   final GlobalKey _formKey = GlobalKey();
 
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    final TextEditingController _emailController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
 
     return Scaffold(
       body: Stack(
@@ -26,40 +29,8 @@ class AuthPage extends StatelessWidget {
               ),
               Stack(
                 children: [
-                  Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            controller: _emailController,
-                            decoration: InputDecoration(
-                              icon: Icon(Icons.account_circle),
-                              labelText: "Email",
-                            ),
-                            validator: (String value) {
-                              if (value.isEmpty) {
-                                return "Please input corrent Email";
-                              }
-                              return null;
-                            },
-                          ),
-                          TextFormField(
-                            controller: _passwordController,
-                            decoration: InputDecoration(
-                              icon: Icon(Icons.account_circle),
-                              labelText: "Password",
-                            ),
-                            validator: (String value) {
-                              if (value.isEmpty) {
-                                return "Please input corrent password";
-                              }
-                              return null;
-                            },
-                          ),
-                          Text("Forgot Password"),
-                        ],
-                      )),
-                  // Container(width:100, height: 50, color: Colors.black,),
+                  _inputForm(size),
+                  _authButton(size),
                 ],
               ),
               Container(
@@ -72,6 +43,72 @@ class AuthPage extends StatelessWidget {
             ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _authButton(Size size) {
+    return Positioned(
+      left: size.width * 0.15,
+      right: size.width * 0.15,
+      bottom: 0,
+      child: RaisedButton(
+          child: Text("Login"),
+          color: Colors.blue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          onPressed: () {}),
+    );
+  }
+
+  Widget _inputForm(Size size) {
+    return Padding(
+      padding: EdgeInsets.all(size.width * 0.05),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        elevation: 6,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 12.0, right: 12, top: 12, bottom: 32),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.account_circle),
+                    labelText: "Email",
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Please input corrent Email";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.vpn_key),
+                    labelText: "Password",
+                  ),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return "Please input corrent password";
+                    }
+                    return null;
+                  },
+                ),
+                Container(
+                  height: 8,
+                ),
+                Text("Forgot Password"),
+              ],
+            ),
+          ),
+          // Container(width:100, height: 50, color: Colors.black,),
+        ),
       ),
     );
   }
